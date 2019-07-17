573f53382f7bb7df106df709881ed931c8cee6d2
meta-sachin-gidwani <52689691+meta-sachin-gidwani@users.noreply.github.com>
2019-07-17 16:22:23 +0530

    Update SplitArrayTest.java

--- ASSIGNMENT4/src/SplitArrayTest.java
+++ ASSIGNMENT4/src/SplitArrayTest.java
@@ -1,23 +1,22 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 
 public class SplitArrayTest {
 
 	@Test
-	public void test1() {
-		assertEquals( 3,ArrOperation.splitArray(new int[]{1, 1, 1, 2, 1}));
-		assertEquals( -1,ArrOperation.splitArray(new int[]{2, 1, 1, 2, 1}));
-		assertEquals( 1,ArrOperation.splitArray(new int[]{10, 10}));
-		assertEquals( -1,ArrOperation.splitArray(new int[]{1}));
+	public void splitArrayNotContainError() {
+		assertEquals( 3, ArrOperation.splitArray( new int[]{1, 1, 1, 2, 1}));
+		assertEquals( -1, ArrOperation.splitArray( new int[]{2, 1, 1, 2, 1}));
+		assertEquals( 1, ArrOperation.splitArray( new int[]{10, 10}));
+		assertEquals( -1, ArrOperation.splitArray( new int[]{1}));
 	}
+	
 	@Test
-	public void test2(){
+	public void splitArrayContainEmptyArray(){
 		try {
 			ArrOperation.splitArray(new int[]{});
 		} catch (AssertionError e) {
-			System.out.println("Array is empty");
+			System.out.println("Array is empty\n" + e);
 		}
 	}
 }
