dcb8b1388c764190e5d780ae413d24502aac14ed
meta-sachin-gidwani <52689691+meta-sachin-gidwani@users.noreply.github.com>
2019-07-22 15:18:22 +0530

    Update PolyTest.java

--- ASSIGNMENT6/src/PolyTest.java
+++ ASSIGNMENT6/src/PolyTest.java
@@ -17,18 +17,19 @@ public class PolyTest {
 
 	@Test
 	public void testAddPoly() {
-		assertArrayEquals(
-				new Poly(new int[] { 4, 8, 4 }).getArray(),
-				Poly.addPoly(new Poly(new int[] { 4, 2, 1 }),
-						new Poly(new int[] { 6, 3 })).getArray());
+		assertArrayEquals(new Poly(new int[] { 4, 8, 4 }).getArray(),
+				  Poly.addPoly(new Poly(new int[] { 4, 2, 1 }),
+			          new Poly(new int[] { 6, 3 })).getArray());
 	}
 
 	@Test
 	public void testMultiplyPoly() {
-		assertArrayEquals(
-				new Poly(new int[] { 1, 4, 7, 6 }).getArray(),
-				Poly.multiplyPoly(new Poly(new int[] { 1, 2, 3 }),
-						new Poly(new int[] { 1, 2 })).getArray());
+		assertArrayEquals(new Poly(new int[] { 1, 4, 7, 6 }).getArray(),
+				           Poly.multiplyPoly(new Poly(new int[] { 1, 2, 3 }),
+				           new Poly(new int[] { 1, 2 })).getArray());
+		assertArrayEquals(new Poly(new int[] { 3, 8, 5, 2 }).getArray(),
+				           Poly.multiplyPoly(new Poly(new int[] { 1, 2 }),
+					   new Poly(new int[] { 3, 2, 1 })).getArray());
 	}
 
 }
