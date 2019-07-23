2272e7b7c840f0d836e215fb43a35de724ff5682
meta-sachin-gidwani <52689691+meta-sachin-gidwani@users.noreply.github.com>
2019-07-23 15:20:31 +0530

    Update SparseMatrixTest.java

--- ASSIGNMENT7/src/SparseMatrixTest.java
+++ ASSIGNMENT7/src/SparseMatrixTest.java
@@ -1,33 +1,53 @@
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class SparseMatrixTest {
+public class SparseMatrixTest {
 
-    @Test
-    void transposeTest() {
-       assertArrayEquals(new int[][] {{1,3},{2,4}},new SparseMatrix(new int[][]{{1,2},{3,4}}).transpose());
-       assertArrayEquals(new int[][] {{6,3,1},{2,10,5}},new SparseMatrix(new int[][]{{6,2},{3,10},{1,5}}).transpose());
-    }
-    
-    @Test
-    void symmetricalTest() {
-        assertTrue(new SparseMatrix(new int[][]{{1,2},{2,1}}).isSymmetrical());
-        assertFalse(new SparseMatrix(new int[][]{{1,3},{2,1}}).isSymmetrical());
-    }
-    
-    @Test
-    void addMatrixTest() {
-        assertArrayEquals(new int[][] {{1,3},{2,4}},new SparseMatrix(new int[][]{{0,2},{1,2}}).addMatrix(new SparseMatrix(new int[][]{{1,1},{1,2}})));
-        assertArrayEquals(new int[][] {{2,4,6},{2,3,4}},new SparseMatrix(new int[][]{{1,2,3},{1,2,3}}).addMatrix(new SparseMatrix(new int[][]{{1,2,3},{1,1,1}})));
-    }
-    
-    @Test
-    void multiplyMatrixTest() {
-        assertArrayEquals(new int[][] {{2,4},{3,5}},new SparseMatrix(new int[][]{{0,2},{1,2}}).multiplyMatrix(new SparseMatrix(new int[][]{{1,1},{1,2}})));
-        assertArrayEquals(new int[][] {{7,11}},new SparseMatrix(new int[][]{{3,4}}).multiplyMatrix(new SparseMatrix(new int[][]{{1,1},{1,2}})));
-    }
+	@Test
+	public void transposeTest() {
+		assertArrayEquals(new int[][]{{1, 3}, {2, 4}}, new SparseMatrix(
+				new int[][]{{1, 2}, {3, 4}}).transpose());
+		assertArrayEquals(new int[][]{{6, 3, 1}, {2, 10, 5}}, new SparseMatrix(
+				new int[][]{{6, 2}, {3, 10}, {1, 5}}).transpose());
+	}
+
+	@Test
+	public void symmetricalTest() {
+		assertTrue(new SparseMatrix(new int[][]{{1, 2}, {2, 1}})
+				.isSymmetrical());
+		assertFalse(new SparseMatrix(new int[][]{{1, 3}, {2, 1}})
+				.isSymmetrical());
+	}
+
+	@Test
+	public void addMatrixTestWithoutError() {
+		assertArrayEquals(new int[][]{{1, 3}, {2, 4}}, new SparseMatrix(
+				          new int[][]{{0, 2}, {1, 2}}).addMatrix(new SparseMatrix(
+				          new int[][]{{1, 1}, {1, 2}})));
+		assertArrayEquals(new int[][]{{2, 4, 6}, {2, 3, 4}}, new SparseMatrix(
+				          new int[][]{{1, 2, 3}, {1, 2, 3}}).addMatrix(new SparseMatrix(
+				          new int[][]{{1, 2, 3}, {1, 1, 1}})));
+	}
+
+	@Test(expected = AssertionError.class)
+	public void addMatrixTestWithError() {
+		new SparseMatrix(new int[][]{{0, 2, 5}, {1, 2, 9}}).addMatrix(new SparseMatrix(new int[][]{{1, 1}, {1, 2}}));
+	}
+
+	@Test
+	public void multiplyMatrixTestWithoutError() {
+		assertArrayEquals(new int[][]{{2, 4}, {3, 5}}, new SparseMatrix(
+				          new int[][]{{0, 2}, {1, 2}}).multiplyMatrix(new SparseMatrix(
+				          new int[][]{{1, 1}, {1, 2}})));
+		assertArrayEquals(new int[][]{{7, 11}}, new SparseMatrix(
+				          new int[][]{{3, 4}}).multiplyMatrix(new SparseMatrix(
+				          new int[][]{{1, 1}, {1, 2}})));
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void multiplyMatrixTestWithError() {
+		new SparseMatrix(new int[][]{{0, 2}, {1, 2}}).addMatrix(new SparseMatrix(new int[][]{{1, 1, 4}, {1, 2, 6}}));
+	}
 
 }
