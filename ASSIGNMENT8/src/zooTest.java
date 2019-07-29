c8f630917c0efd1c49b366eb42e3f7ea98869415
meta-sachin-gidwani <52689691+meta-sachin-gidwani@users.noreply.github.com>
2019-07-29 10:42:19 +0530

    Update zooTest.java

--- ASSIGNMENT8/src/zooTest.java
+++ ASSIGNMENT8/src/zooTest.java
@@ -1,5 +1,4 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class zooTest {
@@ -9,10 +8,10 @@ public class zooTest {
 		Zoo zoo=new Zoo();
 		zoo.addZone("Bird");
 		assertTrue(zoo.addAnimal("SherKhan", "Lion", 120, 10));
-		assertTrue(zoo.addAnimal("mithu", "Parrot", 0.250, 10));
 		assertTrue(zoo.addAnimal("Sheru", "Lion", 120, 10));
-//		Animal animal=(Lion)zoo.zone.get(1).cageList.get(0).animal.get(0);
-//		System.out.println(animal.getSound());
+		assertTrue(zoo.addAnimal("mithu", "Parrot", 0.250, 10));
+		Animal animal=(Lion)zoo.zone.get(1).cageList.get(0).animal.get(0);
+		System.out.println(animal.getSound());
 	}
 	
 	@Test(expected = AssertionError.class)
@@ -22,21 +21,6 @@ public class zooTest {
 		zoo.addZone("Bird");
 		zoo.addZone("Bird");
 		zoo.addAnimal("SherKhan", "Lion", 120, 10);
-	}
-
-	@Test
-	public void addCageWhenZoneIsNotFull() {
-		Zoo zoo=new Zoo();
-		zoo.addZone("Bird");
-		assertTrue(zoo.addCage("Parrot"));
-	}
-	
-	@Test(expected = AssertionError.class)
-	public void addCageWhenZoneIsFull() {
-		Zoo zoo=new Zoo();
-		zoo.addZone("Bird");
-		zoo.addCage("Parrot");
-		zoo.addCage("Eagle");
 	}
 	
 	@Test
