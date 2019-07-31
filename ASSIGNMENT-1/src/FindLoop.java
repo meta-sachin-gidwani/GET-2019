a115f0b02c2a8a88708dbae70ba5322f6c58039e
meta-sachin-gidwani <52689691+meta-sachin-gidwani@users.noreply.github.com>
2019-07-31 18:15:53 +0530

    Add files via upload

---
+++ ASSIGNMENT-1/src/FindLoop.java
@@ -0,0 +1,20 @@
+public class FindLoop {
+	
+	/**
+	 * @param linked list
+	 * @return true if list has loop else false
+	 */
+	public static boolean detectLoop(LinkedList list){
+		Node node1=list.head;
+		Node node2=list.head;
+		while(node1!=null && node2!=null){
+			//check for loop
+			node1=node2.next;
+			node2=node1.next.next;
+			if(node1==node2)
+				return true;
+		}
+		return false;
+	}
+
+}
