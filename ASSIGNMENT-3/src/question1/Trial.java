b6b696904020339ccf0ae74ddf50b5be7ac55385
meta-sachin-gidwani <52689691+meta-sachin-gidwani@users.noreply.github.com>
2019-08-02 20:10:34 +0530

    Add files via upload

---
+++ ASSIGNMENT-3/src/question1/Trial.java
@@ -0,0 +1,11 @@
+package question1;
+
+public class Trial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String s="my name/is";
+       System.out.println(s.substring(s.indexOf("/")+1,s.length()));
+	}
+
+}
