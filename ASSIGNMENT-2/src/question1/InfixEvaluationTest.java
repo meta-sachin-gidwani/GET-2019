01cfd0f1ba963ed414c3f7c9778ff187a31b3523
meta-sachin-gidwani <52689691+meta-sachin-gidwani@users.noreply.github.com>
2019-08-02 16:48:12 +0530

    Update and rename EvaluationTest.java to InfixEvaluationTest.java

--- ASSIGNMENT-2/src/question1/InfixEvaluationTest.java
+++ ASSIGNMENT-2/src/question1/InfixEvaluationTest.java
@@ -6,8 +6,10 @@ public class EvaluationTest {
 
 	@Test
 	public void evaluationWithValidInput() {
-		InfixEvaluation expression = new InfixEvaluation("( 2 + 3 ) / 4 % 5 > 2 || 3 < 1 && 3 > 1 ");
-		assertEquals("false", expression.evaluate());	
+		InfixEvaluation expression1 = new InfixEvaluation("( 2 + 3 ) / 4 % 5 > 2 || 3 < 1 && 3 > 1 ");
+		InfixEvaluation expression2 = new InfixEvaluation("( 2 + 3 ) / 4 ");
+		assertEquals("1", expression2.evaluate());
+		assertEquals("false", expression1.evaluate());	
 	}
 
 	@Test(expected = AssertionError.class)
