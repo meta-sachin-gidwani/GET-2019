e7ad399862e458a74a5e95572bcbb889825fc542
meta-sachin-gidwani <52689691+meta-sachin-gidwani@users.noreply.github.com>
2019-08-08 15:43:32 +0530

    Update EmployeeLinkedListTest.java

--- ASSIGNMENT-5/src/question2/EmployeeLinkedListTest.java
+++ ASSIGNMENT-5/src/question2/EmployeeLinkedListTest.java
@@ -1,17 +1,22 @@
 package question2;
-
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class EmployeeLinkedListTest {
 
 	@Test
 	public void LinkedListSortingTest() {
 		EmployeeLinkedList list = new EmployeeLinkedList();
-	    list.insertEmployee(new Employee("abc", 12, 13));
-	    list.insertEmployee(new Employee("def", 33, 13));
-	    list.insertEmployee(new Employee("vfrf", 25, 50));
-	    InsertionSort sort = new InsertionSort();
-	    sort.sort(list,list.size);
-	    list.print();
-	}
+		list.insertEmployee(new Employee("employee1", 12, 10000));
+		list.insertEmployee(new Employee("employee2", 33, 10000));
+		list.insertEmployee(new Employee("employee3", 25, 5000));
+		list.insertEmployee(new Employee("employee4", 25, 16000));
+		list.insertEmployee(new Employee("employee5", 25, 23000));
+		InsertionSort sort = new InsertionSort();
+		String expected[] = new String[]{"employee5","employee4","employee1","employee2","employee3"};
+		list = sort.sort(list,list.size);
+		String result [] = list.getEmplyeeList();
+		assertArrayEquals(expected, result);
+	}  
 }
+
