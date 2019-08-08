d80ba7fd3e48cf18022f1df7badab459b1d14072
meta-sachin-gidwani <52689691+meta-sachin-gidwani@users.noreply.github.com>
2019-08-08 14:05:55 +0530

    Update DataEntry.java

--- ASSIGNMENT-5/src/question1/DataEntry.java
+++ ASSIGNMENT-5/src/question1/DataEntry.java
@@ -8,14 +8,24 @@ public class DataEntry {
 		this.key = key;
 		this.value = value;
 	}
+	
 	public int getKey() {
 		return key;
 	}
+	
 	public String getValue() {
 		return value;
 	}
+	
+	public void setValue(String value) {
+		this.value = value;
+	}
+
 	public void setKey(int key) {
 		this.key = key;
 	}
 	
+	public String toString(){
+		return String.format("%d\t%s", key, value);
+	}
 }
