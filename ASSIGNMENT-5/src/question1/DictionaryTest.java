fbc34cd28b3f7d84d53c45ef79d41d128f8ef502
meta-sachin-gidwani <52689691+meta-sachin-gidwani@users.noreply.github.com>
2019-08-08 14:18:10 +0530

    Update DictionaryTest.java

--- ASSIGNMENT-5/src/question1/DictionaryTest.java
+++ ASSIGNMENT-5/src/question1/DictionaryTest.java
@@ -1,33 +1,51 @@
 package question1;
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-import java.util.List; 
-
 import org.junit.Test;
 
 public class DictionaryTest {
 
 	@Test
-	public void sortingTest() {
+	public void sortingFullDictionaryTest() {
 		Dictionary d = new Dictionary("file.json");
-		List<DataEntry> list = new ArrayList<DataEntry>();
-		d.addData(33, "hu");
-		d.addData(33, "hu");
-		list = d.getSortedList();
-		for(int i = 0; i<list.size();i++) {
-			System.out.println(list.get(i).getKey() + " " + list.get(i).getValue());
+		d.addData(33, "follow");
+		d.addData(33, "sing");
+		for(DataEntry entry : d.getSortedList()) {
+			System.out.println(entry);
 		}
+		System.out.println();
 	}
+	
+	@Test
+	public void sortingDictionaryInRangeTest() {
+		Dictionary d = new Dictionary("file.json");
+		d.addData(33, "follow");
+		d.addData(33, "sing");
+		for(DataEntry entry : d.getSortedList()) {
+			System.out.println(entry.getKey()+ "  "+entry.getValue());
+		}
+		System.out.println();
+	}
+	
 	@Test
 	public void getValueTest() {
 		Dictionary d = new Dictionary("file.json");
 		assertEquals("vrinda",d.getValue(6));
 	}
+	
 	@Test
 	public void keyNotFoundTest() {
 		Dictionary d = new Dictionary("file.json");
 		assertEquals("key not found",d.getValue(277));
 	}
 	
+	@Test
+	public void removeDataTest() {
+		Dictionary d = new Dictionary("file.json");
+		d.removeData(4);
+		for(DataEntry entry : d.getSortedList()) {
+			System.out.println(entry);
+		}
+		System.out.println();
+	}
 }
