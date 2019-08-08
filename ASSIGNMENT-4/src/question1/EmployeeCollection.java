f747ed7c98c8a8edd776cd90b1a0a01658c49791
meta-sachin-gidwani <52689691+meta-sachin-gidwani@users.noreply.github.com>
2019-08-08 13:12:06 +0530

    Update EmployeeCollection.java

--- ASSIGNMENT-4/src/question1/EmployeeCollection.java
+++ ASSIGNMENT-4/src/question1/EmployeeCollection.java
@@ -1,11 +1,16 @@
 package question1;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class EmployeeCollection {
 	
-	//collection of employee made using a set
-	HashSet<Employee> set = new HashSet<Employee>();
+	//list of employee made using a set
+	List<Employee> list=new ArrayList<Employee>();
+	Set<Integer> set = new HashSet<Integer>();
 	/**
 	 * method adds employee to set
 	 * @param empId of employee
@@ -13,20 +18,20 @@ public class EmployeeCollection {
 	 * @param address of employee
 	 */
 	public void addEmployee(int empId, String name, String address) {
-
 		Employee employee = new Employee(empId, name, address);
-		// to check whether id is unique or not
-		boolean isIdUnique = true;
-		for (Employee emp : set) {
-			if (emp.getEmpId() == empId) {
-				// if id already exist
-				isIdUnique = false;
-				break;
-			}
-		}
-		// if id is unique add employee to set
-		if (isIdUnique) {
-			set.add(employee);
-		}
+		// to check whether id is unique or not if id is unique add employee to set
+		if(set.add(empId))
+			list.add(employee);
+	}
+	
+	public List<Employee> sortById(){
+		Collections.sort(list, new SortById());
+		return list;
+	}
+	
+	public List<Employee> sortByName(){
+		Collections.sort(list, new SortByName());
+		return list;
 	}
 }
+
