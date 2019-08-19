c2a68c0feae37bd9e5bfbd716adb974fb3179725
meta-sachin-gidwani <52689691+meta-sachin-gidwani@users.noreply.github.com>
2019-08-19 11:30:29 +0530

    Update QUESTION-1.sql

--- ASSIGNMENT-2/QUESTION-1.sql
+++ ASSIGNMENT-2/QUESTION-1.sql
@@ -1,11 +1,14 @@
 USE StoreFront;
 
+#display all the table names present in StoreFront.
 SHOW TABLES;
 
 ALTER TABLE items_ordered DROP FOREIGN KEY product_id;
 
+#remove Product table of the StoreFront database.
 DROP TABLE Product;
 
+#Create the Product table again.
 CREATE TABLE Product(
     product_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
