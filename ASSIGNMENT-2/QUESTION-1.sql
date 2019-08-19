b49d6a4212f6cc5eb254b9a6437e68e91d24aa78
meta-sachin-gidwani <52689691+meta-sachin-gidwani@users.noreply.github.com>
2019-08-19 14:01:23 +0530

    Update QUESTION-1.sql

--- ASSIGNMENT-2/QUESTION-1.sql
+++ ASSIGNMENT-2/QUESTION-1.sql
@@ -5,7 +5,9 @@ USE StoreFront;
 */
 SHOW TABLES;
 
-ALTER TABLE items_ordered DROP FOREIGN KEY product_id;
+ALTER TABLE stock DROP FOREIGN KEY Product_Id1;
+
+ALTER TABLE items_ordered DROP FOREIGN KEY product_id2;
 
 /*
 *remove Product table of the StoreFront database.
@@ -27,5 +29,9 @@ CREATE TABLE Product(
         REFERENCES Category (category_id)
 );
 
-ALTER TABLE items_ordered ADD CONSTRAINT `product_id` FOREIGN KEY (product_id)
+ ALTER TABLE stock
+   ADD CONSTRAINT Product_Id1 FOREIGN KEY ( product_Id) REFERENCES Product( product_Id);
+
+
+ALTER TABLE items_ordered ADD CONSTRAINT product_id2 FOREIGN KEY (product_id)
         REFERENCES Product (product_id);
