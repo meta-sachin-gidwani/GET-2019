467413db3e65e11ebf9107343d2ac138b35e70fe
meta-sachin-gidwani <52689691+meta-sachin-gidwani@users.noreply.github.com>
2019-09-03 16:55:06 +0530

    Add files via upload

---
+++ assignment/EadSession7AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.metacube.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadSession7AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EadSession7AssignmentApplication.class, args);
+	}
+
+}
